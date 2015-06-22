36c783dfbf6fd3c5f1b77fbd7086b8827e3973a0
D7na <8877024@gmail.com>
2015-06-22 05:13:38 +0400

    initial

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,40 @@
+// This is a manifest file that'll be compiled into application, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery-1.11.1.min
+//= require ie
+//= require jquery.easing.1.3
+//= require modernizr.custom
+//= require respond.min
+//= require jquery.polyglot.language.switcher
+//= require jquery.gmap.min
+//= require tinynav.min
+//= require jquery.validate.min
+//= require jquery.flexslider-min
+//= require jquery.jcarousel.min
+//= require jquery.ui.totop.min
+//= require jquery.fancybox.pack
+//= require jquery.cycle.all
+//= require mediaelement-and-player.min
+//= require jquery.fitvids
+//= require jquery.tweet
+//= require jquery.touchSwipe.min
+//= require custom
+//= require responsive-tables
+//= require jflickrfeed.min
+//= require jquery.quicksand
+//= require jquery-ui-1.10.1.custom.min
+//= require_tree .
+
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Finesse::Application.config.session_store :cookie_store, key: '_finesse_session'
