f4e44467fef35036a7e18955d65a5d42690e3456
d7na <8877024@gmail.com>
2015-11-09 01:03:55 +0400

    Delete the paperclip

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,11 +1,3 @@
 class Work < ActiveRecord::Base
-	#belongs_to :category
-	has_attached_file 	:photo,
-						:styles => { :medium => "300x300>", 
-									 :thumb  => "100x100>" },
-						:url => "/public/data/:id/:style/:basename.:extension",
-						:path => "#{Rails.root}/public/data/:id/:style/:basename.:extension" 
-	
-	validates_attachment_content_type :photo, :presence => true, :size => { :in => 0..10.megabytes },
-						:content_type => {:content_type => /^image\/(jpeg|png|jpg|gif|tiff)$/}
+
 end
