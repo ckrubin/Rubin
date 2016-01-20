bb2ab2d02418199de0d13299a230f31ae2560c94
Portfolance <8877024@gmail.com>
2016-01-20 07:23:07 +0400

    clean assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Finesse::Application.configure do
   #config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.precompile += %w( *.js *.css )
+  # config.assets.compile = false
+  # config.assets.precompile += %w( *.js *.css )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
