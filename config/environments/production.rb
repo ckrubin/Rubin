a84bd4647ce665fed26e984601bf2c222d4e24d0
Portfolance <8877024@gmail.com>
2016-01-20 08:10:21 +0400

    clean assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Finesse::Application.configure do
   #config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += %w( *.js *.css .svg .eot .woff .ttf)
   # config.assets.paths << Rails.root.join('app','assets','font')
 
